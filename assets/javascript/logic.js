d27aff2a75e061845371dee61ae9f7634d819e7b
sharonpamela <sharon.pamela.santana@gmail.com>
2019-03-12 18:45:18 -0700

    added logic and html code

---
+++ assets/javascript/logic.js
@@ -0,0 +1,167 @@
+// Steps to complete:
+
+// 1. Initialize Firebase
+// 2. Create button for adding new employees - then update the html + update the database
+// 3. Create a way to retrieve employees from the employee database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
+
+// 1. Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD289sGibZ-CP_vaKC0i_fEcWJsivI9dGk",
+    authDomain: "scheduling-a73f8.firebaseapp.com",
+    databaseURL: "https://scheduling-a73f8.firebaseio.com",
+    projectId: "scheduling-a73f8",
+    storageBucket: "",
+    messagingSenderId: "847054533508"
+  };
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
+  // 2. Button for adding Employees
+  $("#submitButton").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    let trainName = $("#trainName").val().trim();
+    let trainDest = $("#trainDest").val().trim();
+    let trainTime = $("#trainTime").val().trim(); //TODO: VALIDATE TIME IS FUTURE AND PROPERLY FORMATED
+    let trainFreq = $("#trainFreq").val().trim();
+
+    function validateTime(){
+
+    // toISOString() will give current UTC Date. 
+    // So to get the current local time we have to get getTimezoneOffset() 
+    // and subtract it from current time
+      // document.getElementById('dt').max = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().split("T")[0];
+    //<input type="date" min='1899-01-01' id="dt" />
+
+
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; //January is 0!
+      var yyyy = today.getFullYear();
+      if(dd<10){
+              dd='0'+dd
+          } 
+          if(mm<10){
+              mm='0'+mm
+          } 
+      
+      today = yyyy+'-'+mm+'-'+dd;
+      document.getElementById("datefield").setAttribute("max", today);
+      }
+  
+    // Creates local "temporary" object for holding train data
+    let newTrain = {
+        name: trainName,
+        dest: trainDest,
+        time: trainTime,
+        freq: trainFreq
+    };
+  
+    // Uploads employee data to the database
+    database.ref("/trains").push(newTrain);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').style.display = 'none';
+    },3000);
+
+    // Logs everything to console
+    // console.log(newTrain.name);
+    // console.log(newTrain.dest);
+    // console.log(newTrain.time);
+    // console.log(newTrain.freq);
+  
+    // Clears all of the text-boxes
+    document.getElementById('trainForm').reset();
+  });
+  
+  // 3. Create Firebase event for adding a train to the database 
+  // so that we can populate the html with new train 
+  database.ref("/trains").on("child_added", function(childSnapshot) {
+    //console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    let trainName = childSnapshot.val().name;
+    let trainDest = childSnapshot.val().dest;
+    let trainTime  = "17:30" // hard code for now childSnapshot.val().time;
+    let trainFreq = childSnapshot.val().freq;
+
+  
+    // Employee Info
+    // console.log(trainName);
+    // console.log(trainDest);
+    // console.log(trainTime);
+    // console.log(trainFreq);
+  
+    // format the time appropiately
+    // let timedate = trainTime.split('T'); //["2017-06-01", "08:30"]
+    // let formattedTime1 = timedate[0] +" "+timedate[1];
+    // console.log(moment(formattedTime1,"X"));
+
+
+    // Calculate the next trains arrival and how many minutes away it is
+    let response = getTrainTime(trainTime,trainFreq)
+
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDest),
+      $("<td>").text(trainFreq),
+      $("<td>").text(response[0]),
+      $("<td>").text(response[1]),
+    );
+  
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+  });
+  
+  function getTrainTime(trainTime,trainFreq){
+
+        // Assumptions
+        let tFrequency = trainFreq;
+
+        // Time of first train
+        let firstTime = trainTime;
+    
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+    
+        // Current Time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+    
+        // Time apart (remainder)
+        var tRemainder = diffTime % tFrequency;
+        console.log(tRemainder);
+    
+        // Minute Until Train
+        var tMinutesTillTrain = tFrequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+        return ([nextTrain,tMinutesTillTrain]);
+  }
+  // Example Time Math
+  // -----------------------------------------------------------------------------
+  // Assume Employee start date of January 1, 2015
+  // Assume current date is March 1, 2016
+  
+  // We know that this is 15 months.
+  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
+  
